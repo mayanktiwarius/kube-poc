a9ec048eff5632d11616248a9491d6dba1a2fe22
mtiwar006 <mayank_tiwari@cable.comcast.com>
2024-01-24 22:09:04 -0500

    Kube cluster creation completed

--- configs/join.sh
+++ configs/join.sh
@@ -1,1 +1,1 @@
-kubeadm join 10.0.0.10:6443 --token uw289m.sipf2a6izn7lsozp --discovery-token-ca-cert-hash sha256:10ad4ec78cf7e1216438c60819732e8237e52b5cd5a3ad5fa4d56232e5d19c9d 
+kubeadm join 10.0.0.10:6443 --token v2jopi.7etwjbb15ux3ugld --discovery-token-ca-cert-hash sha256:675544cb41b600a02265cda3fdc815aac2810419ed764a8bf35f876d7aff23e2 
